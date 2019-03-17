8e9f7e7812ae5a22b3828d902cf853e73ebb6fa6
ranjtih <48471002+ranjtih@users.noreply.github.com>
2019-03-17 10:28:26 +0530

    Create uppertolowerviceversa.c

---
+++ uppertolowerviceversa.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+char a[30];
+int b[30];
+int len,i;
+printf("enter the string\n");
+scanf("%s",a);
+for(len=0;a[len]!='\0';len++);
+
+for(i=0;i<len;i++)
+{
+	if(a[i]>='A'&&a[i]<='Z')
+	{
+		a[i]=a[i]+32;
+	}
+	else if(a[i]>='a'&&a[i]<='z')
+	{
+		a[i]=a[i]-32;
+	}
+}
+printf("%s",a);
+	
+	
+	
+return 0;
+}
